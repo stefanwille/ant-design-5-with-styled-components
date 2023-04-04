ee75e799c3a030f97a94ee73fa46ce45c392997d
Stefan Wille <github.stefanwille@gmail.com>
2023-04-04 11:00:05 +0200

    Remove hash

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,43 @@
 import "antd/dist/reset.css";
-import { App as AntApp, Button, DatePicker, Space, version } from "antd";
+import {
+  App as AntApp,
+  Button,
+  ConfigProvider,
+  DatePicker,
+  Space,
+  version,
+} from "antd";
+import styled from "styled-components";
 
 function App() {
   return (
     <AntApp>
-      <div className="App" style={{ margin: 24 }}>
-        <h1>antd version: {version}</h1>
-        <Space>
-          <DatePicker />
-          <Button type="primary">Primary Button</Button>
-        </Space>
-      </div>
+      <ConfigProvider theme={{ hashed: false }}>
+        <div className="App" style={{ margin: 24 }}>
+          <h1>antd version: {version}</h1>
+          <Space>
+            <DatePicker />
+            <Button type="primary">Primary Button</Button>
+            <Box>Eine Box</Box>
+            <GreenButton>Green Button</GreenButton>
+          </Space>
+        </div>
+      </ConfigProvider>
     </AntApp>
   );
 }
 
 export default App;
+
+const Box = styled.div`
+  border: 4px solid greenyellow;
+  padding: 8px;
+`;
+
+const GreenButton = styled(Button)`
+  background-color: greenyellow;
+  font-size: 30px;
+  height: 60px;
+  text-align: right;
+  width: 200px;
+`;
